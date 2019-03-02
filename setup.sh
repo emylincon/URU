23fa2c1247df8e05879eb9d4437dd9e11a56d03f
Emeka E Ugwuanyi <emylincon@gmail.com>
2019-03-02 21:39:30 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ apt install python3-psutil -y
 apt install python3-matplotlib -y
 apt install python3-pip -y
 pip3 install drawnow
+apt install python3-pyfiglet -y
 clear
 echo 'All done, Ready to Use!!'
